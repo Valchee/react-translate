e9b28f50ecbfdef30a879ae04067e0780e9bddf2
Valchee <matkowski2@gmail.com>
2020-06-21 20:48:04 +0200

    Create LanguageStore component for keeping language 'state'

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: 'english' }
-
-  onLanguageChange = language => {
-    this.setState({ language })
-  }
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i onClick={() => this.onLanguageChange('english')} className="flag us" />
-          <i onClick={() => this.onLanguageChange('polish')} className="flag pl" />
-        </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <ColorContext.Provider value="primary">
+        <LanguageStore>
+          <LanguageSelector />
+
+          <ColorContext.Provider value='primary'>
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
